f4fe95f698b6dd02d0b6f9df792e641743dd95fb
LaraTecSys <61359591+TiagoLaraTecSys@users.noreply.github.com>
2024-10-16 20:08:05 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package goarea
+
+import "math"
+
+// Pi constante
+const Pi = 3.1416
+
+// Circ calcula area de um circulo
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+// Rect calcula area de retangulo
+func Rect(base, altura float64) float64 {
+	return base * altura
+}
+
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+}
